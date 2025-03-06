be792978e2186e4ffc0c3ffeeac2e210c5d4457c
Jagmeet <jagmeetsgh@gmail.com>
2025-03-06 03:35:37 -0500

    yes

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,51 @@
+import React from "react"
+import "./style.css"
+import { Col, Container, Row } from "react-bootstrap"
+
+const Footer = () => {
+  return (
+    <footer>
+        <Container>
+          <Row className="footer-row">
+            <Col md={3} sm={5} className='box'>
+              <div className="logo">
+                  <ion-icon name="bag"></ion-icon>
+                  <h1>XChangeMarket</h1>
+              </div>
+              <p>Sell your products</p>
+            </Col>
+            <Col md={3} sm={5} className='box'>
+              <h2>About Us</h2>
+              <ul>
+                <li>Careers</li>
+                <li>Our Stores</li>
+                <li>Our Cares</li>
+                <li>Terms & Conditions</li>
+                <li>Privacy Policy</li>
+              </ul>
+            </Col>
+            <Col md={3} sm={5} className='box'>
+              <h2>Customer Care</h2>
+              <ul>
+                <li>Help Center </li>
+                <li>How to Buy </li>
+                <li>Track Your Order </li>
+                <li>Corporate & Bulk Purchasing </li>
+                <li>Returns & Refunds </li>
+              </ul>
+            </Col>
+            <Col md={3} sm={5} className='box'>
+              <h2>Contact Us</h2>
+              <ul>
+                <li>68 City Square, Toronto, Canada </li>
+                <li>Email: mpx.inquiries@gmail.com</li>
+                <li>Phone: +1 123 456 789</li>
+              </ul>
+            </Col>
+          </Row>
+        </Container>
+    </footer>
+  )
+}
+
+export default Footer
